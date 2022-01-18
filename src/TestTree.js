50a4cf86d6b1d8ba8764ab0d1b2e20b8291aaa53
Oleg Ivanov <gmiolegv@gmail.com>
2022-01-18 19:35:17 +0300

    Language bar

--- src/TestTree.js
+++ src/TestTree.js
@@ -1,135 +1,136 @@
 import {BarData, Head, MenuItem} from "./sidebar/WrapperSideBar";
-import { BiFolder,BiFolderOpen} from "react-icons/bi";
+import {BiFolder, BiFolderOpen} from "react-icons/bi";
 import React from "react";
-import { MdSubdirectoryArrowRight } from "react-icons/md";
-import { FiAlignJustify } from "react-icons/fi";
-import { BsCaretRight,BsCaretDown } from "react-icons/bs";
+import {MdSubdirectoryArrowRight} from "react-icons/md";
+import {FiAlignJustify} from "react-icons/fi";
+import {BsCaretRight, BsCaretDown} from "react-icons/bs";
 //
-export const imgSize=25;
+export const imgSize = 25;
 
 
-export const imgSizeSm=20;
-export const colorImage="#ca981c"
-export   const barData=new BarData();
-barData.iconTree=<MdSubdirectoryArrowRight size={imgSize} color={colorImage}/>
-barData.iconToggleMenu=<FiAlignJustify size={30} color={colorImage}/>
-barData.imageToggleNode1=<BsCaretRight size={20} color={colorImage}/>
-barData.imageToggleNode2=<BsCaretDown  size={20} color={colorImage}/>
+export const imgSizeSm = 20;
+export const colorImage = "#ca981c"
+export const barData = new BarData();
+barData.iconTree = <MdSubdirectoryArrowRight size={imgSize} color={colorImage}/>
+barData.iconToggleMenu = <FiAlignJustify size={30} color={colorImage}/>
+barData.imageToggleNode1 = <BsCaretRight size={20} color={colorImage}/>
+barData.imageToggleNode2 = <BsCaretDown size={20} color={colorImage}/>
 
-function myHeader(){
- return(<img src="https://ucmrussia.ru/upload/medialibrary/851/851d4df328991d082ec9c66ea5c449f6.png" height={30}/>)
+function myHeader() {
+    return (<img src="https://ucmrussia.ru/upload/medialibrary/851/851d4df328991d082ec9c66ea5c449f6.png" height={30}/>)
 }
-barData.head=new Head(myHeader())
+
+barData.head = new Head(myHeader())
 
 {
-    const mymenu=new MenuItem();
-    mymenu.content="Simple1"
-    mymenu.isShow=true;
-    mymenu.href="sinple1";
-    mymenu.imageSrc=<BiFolder color={colorImage} size={imgSize}/>
+    const mymenu = new MenuItem();
+    mymenu.content = "Simple1"
+    mymenu.isShow = true;
+    mymenu.href = "sinple1";
+    mymenu.imageSrc = <BiFolder color={colorImage} size={imgSize}/>
 
-    mymenu.imageSize=23;
+    mymenu.imageSize = 23;
     barData.menuItems.push(mymenu);
 }
 {
-    const mymenu=new MenuItem();
-    mymenu.content= "Simple my First Menu"
-    mymenu.isShow=true;
-    mymenu.href="Просто2";
-    mymenu.imageSrc=<BiFolder  color={colorImage} size={imgSize}/>
-    mymenu.imageSrcOpen=<BiFolderOpen  color={colorImage} size={imgSize}/>
-    mymenu.imageSize=23;
-    mymenu.tooltip="Simple my First Menu"
+    const mymenu = new MenuItem();
+    mymenu.content = "Simple my First Menu"
+    mymenu.isShow = true;
+    mymenu.href = "Просто2";
+    mymenu.imageSrc = <BiFolder color={colorImage} size={imgSize}/>
+    mymenu.imageSrcOpen = <BiFolderOpen color={colorImage} size={imgSize}/>
+    mymenu.imageSize = 23;
+    mymenu.tooltip = "Simple my First Menu"
     barData.menuItems.push(mymenu);
     {
-        const mi=new MenuItem();
-        mi.content="content1";
-        mi.href="/wwww1"
-        mi.imageSize=imgSize;
-        mi.isShow=true;
-        mi.imageSrc=<BiFolder  color={colorImage} size={imgSizeSm}/>
+        const mi = new MenuItem();
+        mi.content = "content1";
+        mi.href = "/wwww1"
+        mi.imageSize = imgSize;
+        mi.isShow = true;
+        mi.imageSrc = <BiFolder color={colorImage} size={imgSizeSm}/>
 
         mymenu.menuItems.push(mi);
     }
     {
-        const mi=new MenuItem();
-        mi.content="content2";
-        mi.href="/wwww2"
-        mi.imageSize=imgSize;
-        mi.isShow=true;
-        mi.imageSrc=<BiFolder  color={colorImage} size={imgSizeSm}/>
+        const mi = new MenuItem();
+        mi.content = "content2";
+        mi.href = "/wwww2"
+        mi.imageSize = imgSize;
+        mi.isShow = true;
+        mi.imageSrc = <BiFolder color={colorImage} size={imgSizeSm}/>
         mymenu.menuItems.push(mi);
     }
     {
-        const mi=new MenuItem();
-        mi.content="content3";
-        mi.href="/wwww3"
-        mi.isShow=true;
-        mi.imageSize=imgSize;
-        mi.imageSrc=<BiFolder  color={colorImage} size={imgSizeSm}/>
+        const mi = new MenuItem();
+        mi.content = "content3";
+        mi.href = "/wwww3"
+        mi.isShow = true;
+        mi.imageSize = imgSize;
+        mi.imageSrc = <BiFolder color={colorImage} size={imgSizeSm}/>
 
         mymenu.menuItems.push(mi);
     }
 }
 {
-    const mymenu=new MenuItem();
-    mymenu.content="simple3"
-    mymenu.isShow=true;
-    mymenu.imageSrc=<BiFolder color={colorImage} size={imgSize}/>
-    mymenu.imageSrcOpen=<BiFolderOpen  color={colorImage} size={imgSize}/>
-    mymenu.imageSize=23;
-    mymenu.href="Sinple3";
+    const mymenu = new MenuItem();
+    mymenu.content = "simple3"
+    mymenu.isShow = true;
+    mymenu.imageSrc = <BiFolder color={colorImage} size={imgSize}/>
+    mymenu.imageSrcOpen = <BiFolderOpen color={colorImage} size={imgSize}/>
+    mymenu.imageSize = 23;
+    mymenu.href = "Sinple3";
     {
-        const mi=new MenuItem();
-        mi.content="Content221";
-        mi.href="/wwww221"
-        mi.imageSize=imgSize;
-        mi.imageSrc=<BiFolder  color={colorImage} size={imgSizeSm}/>
+        const mi = new MenuItem();
+        mi.content = "Content221";
+        mi.href = "/wwww221"
+        mi.imageSize = imgSize;
+        mi.imageSrc = <BiFolder color={colorImage} size={imgSizeSm}/>
         mymenu.menuItems.push(mi);
     }
     {
-        const mi=new MenuItem();
-        mi.content="Content222";
-        mi.href="/wwww222"
-        mi.imageSize=imgSize;
-        mi.imageSrc=<BiFolder  color={colorImage} size={imgSizeSm}/>
+        const mi = new MenuItem();
+        mi.content = "Content222";
+        mi.href = "/wwww222"
+        mi.imageSize = imgSize;
+        mi.imageSrc = <BiFolder color={colorImage} size={imgSizeSm}/>
         mymenu.menuItems.push(mi);
     }
 
 
     {
-        const mi=new MenuItem();
-        mi.content="Content223";
-        mi.href="/wwww223"
-        mi.imageSize=imgSize;
-        mi.imageSrc=<BiFolder  color={colorImage} size={imgSizeSm}/>
+        const mi = new MenuItem();
+        mi.content = "Content223";
+        mi.href = "/wwww223"
+        mi.imageSize = imgSize;
+        mi.imageSrc = <BiFolder color={colorImage} size={imgSizeSm}/>
 
         mymenu.menuItems.push(mi);
 
     }
     {
-        const mi=new MenuItem();
-        mi.content=" My Route";
-        mi.href="/wwww221"
-        mi.imageSize=imgSize;
-        mi.imageSrc=<BiFolder color={colorImage} size={imgSizeSm}/>
+        const mi = new MenuItem();
+        mi.content = " My Route";
+        mi.href = "/wwww221"
+        mi.imageSize = imgSize;
+        mi.imageSrc = <BiFolder color={colorImage} size={imgSizeSm}/>
         mymenu.menuItems.push(mi);
     }
     {
-        const mi=new MenuItem();
-        mi.content="My Route 2";
-        mi.href="/wwww22"
-        mi.imageSize=imgSize;
-        mi.imageSrc=<BiFolder  color={colorImage} size={imgSizeSm}/>
+        const mi = new MenuItem();
+        mi.content = "My Route 2";
+        mi.href = "/wwww22"
+        mi.imageSize = imgSize;
+        mi.imageSrc = <BiFolder color={colorImage} size={imgSizeSm}/>
         mymenu.menuItems.push(mi);
     }
 
     {
-        const mi=new MenuItem();
-        mi.content="MyRoute 3";
-        mi.href="/wwww23"
-        mi.imageSize=imgSize;
-        mi.imageSrc=<BiFolder color={colorImage} size={imgSizeSm}/>
+        const mi = new MenuItem();
+        mi.content = "MyRoute 3";
+        mi.href = "/wwww23"
+        mi.imageSize = imgSize;
+        mi.imageSrc = <BiFolder color={colorImage} size={imgSizeSm}/>
 
         mymenu.menuItems.push(mi);
 
--- src/index.js
+++ src/index.js
@@ -1,104 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {useState} from 'react';
+import { render } from "react-dom";
+import { AppProvider } from "./js/context";
+
+import Wrapper from "./components/Wrapper";
+import "./js/i18";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
-import WrapperSideBar, {MenuItem} from "./sidebar/WrapperSideBar";
-//import { FaRoute} from 'react-icons/fa';,imgSize
-//import {barData,imgSizeSm,colorImage} from "./TestTree"
-import {barData,imgSizeSm,colorImage} from "./TestMenu"
+import "./testMenu.css";
 import './index.css';
+import {getCookie} from "./js/methods";
 
-import { BiFolder,BiFolderOpen} from "react-icons/bi";
-//import "./sidebar/expSide.css"
-import "./testMenu.css"
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw_audio.js", { scope: "/" });
+    });
+}
 
-
-const cont = {
-    name: "not selected"
+// Определение языковой локали
+let config = {
+    language: "ru",
+    country: "RU",
 };
 
-barData.on("onclick",(menuitem)=>{
-    cont.name=menuitem.href;
-    const state = { 'page_id': 1, 'user_id': 5 }
-    const title = ''
+let client = window.navigator ? window.navigator.language : config.language;
 
+let language =
+    client.search("-") > 0
+        ? client.substring(0, client.search("-")).toLowerCase()
+        : client.toLowerCase();
 
-
-    window.history.replaceState(state, title, menuitem.href)
-    initContent(cont)
-    //menuitem.isShow=false;
-    //barData.forceUpdate();
-})
-
-function  initContent(props){
-    const element = (
-        <div>
-            <h2 className="myClassName">{props.name}</h2>
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById('content'));
-}
+// create storage
+// store.dispatch(changeLanguage(language));
+localStorage.setItem('lng', language);
 
 window.onpopstate = function(event) {
     alert(`location: ${document.location}, state: ${JSON.stringify(event.state)}`)
 }
 
+// store.dispatch(changeLanguage(language));
 
+function Main () {
 
-const d=document.getElementById('bt1');
-d.onclick=function (){
-    {
-        const mi=new MenuItem();
-        mi.content="Test-22";
-        mi.href="/test22"
-        mi.imageSize=10;
-        mi.imageSrc=<BiFolder size={imgSizeSm} color={colorImage}/>
-        mi.imageSrcOpen=<BiFolderOpen size={imgSizeSm} color={colorImage}/>
-        {
-            const mi1=new MenuItem();
-            mi1.content="Test-22";
-            mi1.href="/test22"
-            mi1.imageSize=20;
-            mi1.imageSrc=<BiFolder size={imgSizeSm} color={colorImage}/>;
-            mi.imageSrcOpen=<BiFolderOpen size={imgSizeSm} color={colorImage}/>
-            mi.menuItems.push(mi1);
-        }
-        barData.menuItems[1].menuItems.push(mi)
-    }
+    const [lang, setLang] = useState(
+        getCookie('language')
+            ? getCookie('language')
+            : 'ru'
+    );
 
-    barData.forceUpdate()
-}
-const d2=document.getElementById('bt2');
-const st={click:true}
-d2.onclick=()=>{
-    barData.openWidth = st.click ? 0 : 300;
-    st.click=!st.click;
-    barData.forceUpdate()
-};
-
-const d3=document.getElementById('bt3');
-d3.onclick=function (){
-    barData.menuItems[1].content="Test"
-    barData.forceUpdate();
+    return (
+        <React.StrictMode>
+            <AppProvider value={{lang, setLang}}>
+                <Wrapper />
+            </AppProvider>
+        </React.StrictMode>
+    )
 }
 
+render(
+    <Main />,
+    document.getElementById("wrapper")
+);
 
-
-
-// barData.closeWidth=60;// ширина зарытого
-// barData.openWidth=700;// ширина открытого
-// barData.isOpen=true;  // состояние  - открыто
-
-
-barData.closeWidth=60;// ширина зарытого
-barData.openWidth=280;// ширина открытого
-barData.isOpen=true;  // состояние  - открыто
-
-/**
- * создаем меню
- */
-new WrapperSideBar(barData,"root");
-
-initContent(cont)
-
-reportWebVitals();
+// reportWebVitals();
